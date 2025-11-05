8a30925daf4c86c21dcf338004ea441a7082fdbc
ariftri1000@gmail.com <ariftri1000@gmail.com>
2025-11-05 09:43:31 +0700

    menambah halaman chatbot

---
+++ lib/chatbotpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Chatbotpage extends StatelessWidget {
+  const Chatbotpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Chatbot'),
+      )
+    );
+  }
+}
