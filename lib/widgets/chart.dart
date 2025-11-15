55bd6bde97ce65bd269241b856a2c0de77699261
Diyas <diyasruwandi273@gmail.com>
2025-11-15 10:05:05 +0700

    save modified pubspec.yaml

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LineChartSample extends StatelessWidget {
+  const LineChartSample({super.key, 
+  required this.isShowingMainData,
+  });
+  final bool isShowingMainData;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
